625634e995a87c5e9032f8f8d0e06b960817d023
shogun_dev <brunoadsjp@gmail.com>
2022-08-29 20:30:57 -0300

    add server port in vite config

---
+++ run.sh
@@ -0,0 +1,13 @@
+# necessary node and npm installed
+# create project with vite
+$ npm create vite@latest
+
+project name: <name-your-project>
+framework: react
+variant: react
+
+# install dependencies
+$ npm i
+
+# rodando o projeto
+$ npm run dev
